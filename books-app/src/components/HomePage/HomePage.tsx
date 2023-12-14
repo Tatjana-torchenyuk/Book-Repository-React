856257daf6ffae41ba2d1f6d4502f73499cc6193
Tatjana Torchenyuk <tatjana.torchenyuk@student.ap.be>
2023-12-14 14:37:09 +0000

    Added HomePage content (img, text) & css

--- books-app/src/components/HomePage/HomePage.tsx
+++ books-app/src/components/HomePage/HomePage.tsx
@@ -1,15 +1,17 @@
 import styles from "./HomePage.module.css";
-import bookcaseImg from "./assets/bookcase-portal.jpeg";
+import portal from "./assets/bookcase-portal.jpeg";
 
 const HomePage = () => {
 
     return (
         <main>
+            <h1>Welcome to BookVerse</h1>
             <figure>
-                <img src={bookcaseImg} alt="Image of bookcase with portal" />
+                <a href="/books"><img src={portal} className={styles.portal} alt="Image of bookcase with portal"/></a>
             </figure>
-            <section>
-                Welcome to BookVerse.
+            <section className={styles.introHome}>
+                <p>Immerse yourself in the magical world of literature and explore a treasure trove of books based on authors, publication years, and genres.</p>
+                <p>Whether you're searching for timeless classics, the latest releases, or a specific genre that ignites your passion, BookVerse has it all with just a few clicks.</p>
             </section>
         </main>
     );
