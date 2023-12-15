96f05fccfa08e645e23230d83b9aa7611386a854
Tatjana Torchenyuk <tatjana.torchenyuk@student.ap.be>
2023-12-15 22:20:18 +0000

    Added more content and css to BookListPage component

--- books-app/src/components/BookListPage/BookListPage.tsx
+++ books-app/src/components/BookListPage/BookListPage.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Book } from "../../types";
 import styles from "./BookListPage.module.css";
 
@@ -9,16 +10,27 @@ const BookListPage = ({ books }: BooksListPageProps) => {
 
     return (
         <main>
-            <h1>Books</h1>
-            <section className="bookFilter">
-                <h2>Filter</h2>
-            </section>
-            <section>
-                {books.map(book => {
-                    return <figure>
-                        <img src={book.volumeInfo?.imageLinks.thumbnail}></img>
-                    </figure>
-                })}
+            <h1>All books</h1>
+            <p>New, interesting, and unexpected books you don't want to miss.</p>
+            <section className={styles.booksContainer}>
+                <div className={styles.bookFilter}>
+                    <h2>Filter</h2>
+                </div>
+                <div className={styles.bookList}>
+                    {books.map((book, index) => {
+                        return <article key={index}>
+                            <figure>
+                                <Link to={`/books/${book.id}`}>
+                                    <img src={book.volumeInfo?.imageLinks.thumbnail} alt={book.volumeInfo?.title || 'Book Cover'} ></img>
+                                </Link>
+                            </figure>
+                            <div>
+                                <Link to={`/books/${book.id}`}>{book.volumeInfo?.title}</Link>
+                                <p>by {book.volumeInfo?.authors}</p>
+                            </div>
+                        </article>
+                    })}
+                </div>
             </section>
         </main>
     )
