172db745411225b128efcde78dd3231e554b18b7
Tatjana Torchenyuk <tatjana.torchenyuk@student.ap.be>
2023-12-16 20:34:26 +0000

    Added comments to App.tsx, added API call and comments in BookDetail.tsx

--- books-app/src/App.tsx
+++ books-app/src/App.tsx
@@ -7,15 +7,24 @@ import { useEffect, useState } from "react";
 import { Book, BookRoot } from "./types";
 
 function App() {
-
+  // Retrieved data is kept in state.
   const [books, setBooks] = useState<Book[]>([]);
 
   const loadBooks = async () => {
+    // API-specific query parameters:
+    // - required parameter 'q', using keyword 'subject' to narrow the results where 'fiction' is listed in the category list of the volume,
+    // - optional parameter 'maxResults', to get the maximum allowable number of 40 books,
+    // - optional parameter 'printType', to further narrow the results to only books,
+    // - optional parameter 'langRestrict', restricts the volumes returned to volumes in English
+    // - optional parameter 'orderBy', to get the books with the latest publishedDate.
+
     let response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=subject:fiction&maxResults=40&key=${import.meta.env.VITE_API_KEY_BOOKS}&langRestrict=en&printType=books&orderBy=newest`);
-    let BookRoot: BookRoot = await response.json();
+    let BookRoot: BookRoot = await response.json(); 
     setBooks(BookRoot.items);
   }
 
+  // Hook -> API call (side effect) is disconnected from the rest of our rendering code 
+  // and the empty dependency array ensures that it is executed once after the 1st render
   useEffect(() => {
     loadBooks();
   }, []);
@@ -40,8 +49,6 @@ function App() {
       ]
     }
   ])
-
-  console.log();
 
   return (
     <>
--- books-app/src/components/BookDetailPage/BookDetail.tsx
+++ books-app/src/components/BookDetailPage/BookDetail.tsx
@@ -1,10 +1,36 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import styles from "./BookDetail.module.css";
+import { Book } from "../../types";
+
 
 const BookDetail = () => {
+  // get param from URL
+  const { id } = useParams();
+  const [currentBook, setCurrentBook] = useState<Book>();
 
+  const loadBookById = async (id: string) => {
+    let response = await fetch(`https://www.googleapis.com/books/v1/volumes/${id}`);
+    let book: Book = await response.json();
+    setCurrentBook(book);
+  }
+  // Hook -> API call (side effect) is disconnected from the rest of our rendering code 
+  // and the id must be included in the dependency array so that our callback func is called every time it changes.
+  useEffect(() => {
+    if (id) {
+      loadBookById(id);
+    }
+  }, [id]);
+  
+  console.log(currentBook?.volumeInfo?.description);
+
+  if (!currentBook) {
+    <p>Loading...</p>
+  }
   return (
     <main>
       This is the detail page.
+      {currentBook?.volumeInfo?.description}
     </main>
   )
 }
