86a2a6ce23173a9cb5ae77bf4c0859d61fdd40d9
Tatjana Torchenyuk <tatjana.torchenyuk@student.ap.be>
2023-12-16 12:20:07 +0000

    Adjusted indentation to default 2 spaces

--- books-app/src/components/BookDetailPage/BookDetailPage.tsx
+++ books-app/src/components/BookDetailPage/BookDetailPage.tsx
@@ -2,11 +2,11 @@ import styles from "./BookDetailPage.css";
 
 const BookDetailPage = () => {
 
-    return (
-        <main>
-            This is the detail page.
-        </main>
-    )
+  return (
+    <main>
+      This is the detail page.
+    </main>
+  )
 }
 
 export default BookDetailPage;
--- books-app/src/types.ts
+++ books-app/src/types.ts
@@ -1,29 +1,29 @@
 
 export interface BookRoot {
-    items: Book[]
+  items: Book[]
 }
 
 export interface Book {
-    id: string;
-    selflink?: string;
-    volumeInfo?: BookDetail;
+  id: string;
+  selflink?: string;
+  volumeInfo?: BookDetail;
 }
 
 export interface BookDetail {
-    title: string;
-    authors: string[];
-    publisher: string;
-    publishedDate: Date;
-    description: string;
-    industryIdentifiers: ISBN[];
-    pageCount: number;
-    categories: string[];
-    imageLinks: {
-        thumbnail: string
-    }
+  title: string;
+  authors: string[];
+  publisher: string;
+  publishedDate: Date;
+  description: string;
+  industryIdentifiers: ISBN[];
+  pageCount: number;
+  categories: string[];
+  imageLinks: {
+    thumbnail: string
+  }
 }
 
 export interface ISBN {
-    type: string;
-    identifier: string;
+  type: string;
+  identifier: string;
 }
