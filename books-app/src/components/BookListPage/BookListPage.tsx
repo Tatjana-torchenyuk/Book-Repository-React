283754d8504c9a45e5ad3b35cfd8c2f9844d9db7
Tatjana Torchenyuk <tatjana.torchenyuk@student.ap.be>
2023-12-16 12:21:18 +0000

    Adjusted default indentation to 2 spaces

--- books-app/src/components/BookListPage/BookListPage.tsx
+++ books-app/src/components/BookListPage/BookListPage.tsx
@@ -3,37 +3,37 @@ import { Book } from "../../types";
 import styles from "./BookListPage.module.css";
 
 interface BooksListPageProps {
-    books: Book[]
+  books: Book[]
 }
 
 const BookListPage = ({ books }: BooksListPageProps) => {
 
-    return (
-        <main>
-            <h1>All books</h1>
-            <p>New, interesting, and unexpected books you don't want to miss.</p>
-            <section className={styles.booksContainer}>
-                <div className={styles.bookFilter}>
-                    <h2>Filter</h2>
-                </div>
-                <div className={styles.bookList}>
-                    {books.map((book, index) => {
-                        return <article key={index}>
-                            <figure>
-                                <Link to={`/books/${book.id}`}>
-                                    <img src={book.volumeInfo?.imageLinks.thumbnail} alt={book.volumeInfo?.title || 'Book Cover'} ></img>
-                                </Link>
-                            </figure>
-                            <div>
-                                <Link to={`/books/${book.id}`}>{book.volumeInfo?.title}</Link>
-                                <p>by {book.volumeInfo?.authors}</p>
-                            </div>
-                        </article>
-                    })}
-                </div>
-            </section>
-        </main>
-    )
+  return (
+    <main>
+      <h1>All books</h1>
+      <p>New, interesting, and unexpected books you don't want to miss.</p>
+      <section className={styles.booksContainer}>
+        <div className={styles.bookFilter}>
+          <h2>Filter</h2>
+        </div>
+        <div className={styles.bookList}>
+          {books.map((book, index) => {
+            return <article key={index}>
+              <figure>
+                <Link to={`/books/${book.id}`}>
+                  <img src={book.volumeInfo?.imageLinks.thumbnail} alt={book.volumeInfo?.title || 'Book Cover'} ></img>
+                </Link>
+              </figure>
+              <div>
+                <Link to={`/books/${book.id}`}>{book.volumeInfo?.title}</Link>
+                <p>by {book.volumeInfo?.authors}</p>
+              </div>
+            </article>
+          })}
+        </div>
+      </section>
+    </main>
+  )
 }
 
 export default BookListPage;
